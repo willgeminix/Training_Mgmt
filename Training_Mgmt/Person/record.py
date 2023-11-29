5f79220150c83f27e1027685318f0a8c1002597e
unknown <willxu2023@outlook.com>
2023-11-29 15:04:16 -0800

    finish the record module of Person

---
+++ Training_Mgmt/Person/record.py
@@ -0,0 +1,104 @@
+import datetime
+import pandas as pd
+import pygal
+from IPython.display import display
+from IPython.core.display import HTML
+
+class Record(pd.DataFrame):
+
+    def __init__(self, nutrition):
+        super().__init__(nutrition)
+        self.__record = nutrition
+        
+    def add(self, date, protein, carbon, fat):
+        calorie = protein/4 + carbon/4 + fat/9
+        new_record = pd.Series({"Date": pd.to_datetime(date), "Calorie": calorie, "Protein": protein, "Carbon": carbon, "Fat": fat})
+        self.__record = self.__record.append(new_record)
+        return self.__record
+        
+    def remove(self, date):
+        
+        record_to_remove = self.__record[self.__record["Date"]==pd.to_datetime(date)]
+        if record_to_remove.empty:
+            return "The record does NOT exist!"
+        else:
+            print("Are you sure you want to delete the following record?\n",record_to_remove)
+            
+        while True:
+            check = input("""Enter [1] for "YES"/ [2] for "NO" ("q" to quit):""")
+            if check == "1":
+                break
+            elif check == "2" or check == "q":
+                return None
+            else:
+                print("Invalid input, please re-enter.")
+                continue
+                
+        self.__record = self.__record[~(self.__record["Date"] == pd.to_datetime(date))]
+        return self.__record
+        
+    def modify(self, date, calorie, protein, carbon, fat):
+        
+        record_to_modify = self.__record[self.__record["Date"]==pd.to_datetime(date)]
+        if record_to_modify.empty:
+            return "The record does NOT exist!"
+        else:
+            print("The record you are going to change:\n", record_to_modify)
+            
+        while True:
+            check = input("""Enter [1] for "YES"/ [2] for "NO" ("q" to quit):""")
+            if check == "1":
+                break
+            elif check == "2" or check == "q":
+                return None
+            else:
+                print("Invalid input, please re-enter.")
+                continue
+        
+        if calorie != '':
+            self.__record.loc[row_to_modify.index[0], ("Calorie",)] = calorie
+        if protein != '':
+            self.__record.loc[row_to_modify.index[0], ("Protein",)] = protein
+        if carbon != '':
+            self.__record.loc[row_to_modify.index[0], ("Carbon",)] = carbon
+        if fat != '':
+            self.__record.loc[row_to_modify.index[0], ("Fat",)] = fat
+            
+        return self.__record
+    
+    
+    def show(self, start_date, end_date, indicator=1):
+        
+        condition = (self.__record["Date"] >= pd.to_datetime(start_date)) & (self.__record["Date"] <= pd.to_datetime(end_date))
+        row_to_display = self.__record[condition]
+        if row_to_display.empty:
+            return "There is NO record between the selected dates." 
+        
+        # indicator == 1 means the user wants to display the record of Calories
+        if indicator == 1:
+            graph = pygal.Bar()
+            graph.title = "Calorie Consumption Between {} and {}".format(start_date, end_date)
+            graph.add("Calorie", row_to_display["Calorie"])
+            graph.x_labels = row_to_display["Date"]
+            
+        # indicator != 1 means the user wants to display the record of other nutritions
+        else:
+            # pie chart will be used to display the nutritions of a single day
+            if len(row_to_display) == 1:
+                graph = pygal.Pie()
+                graph.title = "Nutrition Consumption On {} (in g)".format(row_to_display["Date"].values[0])
+                graph.add("Protein", row_to_display["Protein"].values[0])
+                graph.add("Carbon", row_to_display["Carbon"].values[0])
+                graph.add("Fat", row_to_display["Fat"].values[0])
+                
+            # stackedline will be used to display the nutritions of multiple days
+            else:
+                graph = pygal.StackedLine(fill=True)
+                graph.title = "Nutrition Consumption From {} to {} (in g)".format(start_date, end_date)
+                for each in row_to_display.keys()[2:]:
+                    graph.add(each, row_to_display[each].to_list())
+                graph.x_labels = row_to_display["Date"].to_list()
+        
+        # display the graph
+        graph_svg = graph.render(is_unicode=True)
+        display(HTML(graph_svg))
