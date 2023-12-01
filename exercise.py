23a1846ec6ea538d56f20368b8dfdb3744ab7f54
Bingshen Yang <bingshenyang@Bingshens-MacBook-Pro.local>
2023-12-01 00:47:35 -0800

    skeleton

--- exercise.py
+++ exercise.py
@@ -4,11 +4,86 @@ import pandas as pd
 
 class Exercise:
 
-    def __init__(self, intensity_level):
+    def __init__(self, intensity_level, body_part):
         # intensity_level is how hard the user want to train
         self.__intensity_level = intensity_level
-        strength_list = []
-        cardio_list = []
+        self.__body_part = body_part
+        chest_list = [
+            "bench press",
+            "incline bench press",
+            "dumbell flat bench press",
+            "dumbell incline bench press",
+            "dip push up",
+            "pec deck flys",
+            "dumbbell flys",
+            "chest press",
+            "cable crossover",
+            "push-up variations",
+            "svend press",
+            "guillotine press"
+        ]
+        shoulder_list = [
+            "overhead press",
+            "lateral raise",
+            "front raise",
+            "rear delt fly",
+            "arnold press",
+            "upright row",
+            "face pull",
+            "shrugs"
+        ]
+        back_list = [
+            "pull-ups",
+            "lat pulldown",
+            "bent-over row",
+            "deadlift",
+            "t-bar row",
+            "seated cable row",
+            "single-arm dumbbell row",
+            "back extension"
+        ]
+        leg_list = [
+            "squats",
+            "lunges",
+            "deadlifts",
+            "leg press",
+            "leg extension",
+            "leg curl",
+            "calf raises",
+            "step-ups"
+        ]
+        triceps_list = [
+            "triceps pushdown",
+            "overhead triceps extension",
+            "skull crushers",
+            "close grip bench press",
+            "dips",
+            "diamond push-ups",
+            "triceps kickback",
+            "single-arm triceps extension"
+        ]
+        biceps_list = [
+            "bicep curl",
+            "hammer curl",
+            "preacher curl",
+            "concentration curl",
+            "cable curl",
+            "chin-up",
+            "reverse curl",
+            "incline dumbbell curl"
+        ]
+        cardio_list = [
+            "treadmill running or walking",
+            "indoor cycling on stationary bike",
+            "rowing machine",
+            "stair climber machine",
+            "elliptical trainer",
+            "group fitness classes (e.g., Zumba, spinning, aerobics)",
+            "step mill",
+            "assault bike",
+            "arc trainer",
+            "versa climber"
+        ]
 
     @property
     def intensity_level(self):
@@ -17,3 +92,11 @@ class Exercise:
     @intensity_level.setter
     def intensity_level(self, value):
         self.intensity_level = value
+
+    @property
+    def body_part(self):
+        return self.__body_part
+
+    @body_part.setter
+    def __body_part(self, value):
+        self.__body_part = value
---
+++ exercise_suggestion.py
@@ -0,0 +1,11 @@
+import exercise
+import random
+exercise = exercise.Exercise("easy", "chest")
+
+
+def strength_suggestion():
+    random.sample(exercise.chest_list, 6)
+
+
+def cardio_suggestion():
+    random.sample(exercise.cardio_list, 6)
