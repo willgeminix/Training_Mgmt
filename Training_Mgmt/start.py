9e9cad18d2e319a4ad39d7b9c11e4cffbaeae876
unknown <willxu2023@outlook.com>
2023-12-01 14:31:02 -0800

    add the user create function

--- Training_Mgmt/start.py
+++ Training_Mgmt/start.py
@@ -10,9 +10,28 @@ def start():
         record_nutrition = pd.read_csv(os.getcwd()+"\\"+name+"_nutrition.csv", header=True)
         record_exercise = pd.read_csv(os.getcwd()+"\\"+name+"_exercise.csv", header=True)
     except:
-        print('The user does NOT exist. Please create the user first and restart.')
-        create()
-    person = Person.Person(basic_info["name"], basic_info["bdate"], basic_info['height'], basic_info['weight'], basic_info['gender'], basic_info['purpose'])
+        print('The user does NOT exist')
+        check = input("Do you want to create the user? ['1' for YES/'q' to quit/any for NO]:")
+        if check == 1:
+            print('Please create your user.')
+            #create the user record file
+            person_data = create()
+            person_data.to_csv(os.getcwd()+"\\"+person_data['Name'][0]+"_person.csv", index=False)
+            #create the nutrition record file
+            df_nutrition = pd.DataFrame({'Date':[], 'Calorie':[], 'Protein':[], 'Carbon':[], 'Fat':[]})
+            df_nutrition.to_csv(os.getcwd()+"\\"+person_data['Name'][0]+"_nutrition.csv", index=False)
+            #create the exercise record file
+            df_exercise = pd.DataFrame()
+            df_exercise.to_csv(os.getcwd()+"\\"+person_data['Name'][0]+"_exercise.csv", index=False)
+            start()
+        elif check == 'q':
+            print('Thank you for using, see you!')
+            return None
+        else:
+            print("Please try another unser name.")
+            start()
+
+    person = Person.Person(basic_info["Name"], basic_info["Birthdate"], basic_info['Height'], basic_info['Weight'], basic_info['Gender'], basic_info['Purpose'], basic_info['Frequncy'])
 
 
 def create():
@@ -63,3 +82,40 @@ def create():
             break
         except:
             print("The weight should be numeric, please enter again")
+
+    while True:
+        gender = input("Please enter the gender['F'/'M']:")
+        if gender not in ["F", "M"]:
+            print("The gender is NOT correct, please check and try again.")
+            continue
+        elif gender == "F":
+            gender = "Female"
+            break
+        else:
+            gender = "Male"
+            break
+
+    while True:
+        purpose = input("Please enter the purpose ['1' for bulking, '2' for cutting]"):
+        if purpose not in ['1', '2']:
+            print("The purpose is NOT correct, please check and try again.")
+            continue
+        elif purpose == '1':
+            purpose = 'Bulking'
+            break
+        else:
+            purpose = 'Cutting'
+            break
+
+    while True:
+        freq = input("Please enter the frequecy of exercise.\n'1': No exercise\n'2': 1-2 times per week\n'3': 3-4 times per week\n'4': 5-6 times per week\n'5': more than 6 times per week")
+        if freq not in ['1', '2', '3', '4', '5']:
+            print('The frequency is NOT correct, please check and try again.')
+            continue
+        else:
+            freq_dict = {'1':'No exercise', '2':'Lightly exercise', '3':'Moderately active', '4':'Very active', '5':'Super active'}
+            freq = freq_dict[freq]
+            break
+
+    person_data = pd.DataFrame({'Name':[name], 'Birthdate':[bdate], 'Height':[height], 'Weight':[weight], 'Gender':[gender], 'Purpose':[purpose], 'Frequency':[freq]})
+    return person_data
