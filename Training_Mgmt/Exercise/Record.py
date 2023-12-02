f87323caa7b254c38e6ef6dc475d8d9b47fa8ddc
Bingshen Yang <bingshenyang@Bingshens-MacBook-Pro.local>
2023-12-01 17:29:03 -0800

    record

---
+++ Training_Mgmt/Exercise/Record.py
@@ -0,0 +1,107 @@
+import datetime
+import pandas as pd
+import pygal
+from IPython.display import display
+from IPython.core.display import HTML
+import os
+
+
+class Record():
+
+    def __init__(self):
+        self.csv_file = 'example.csv'
+
+    def add(self, list_of_dicts):
+        if not list_of_dicts:
+            return
+
+        # Create a DataFrame from the list of dictionaries
+        df = pd.DataFrame(list_of_dicts)
+
+        # Check if the file exists
+        file_exists = os.path.isfile(self.csv_file)
+
+        # If file exists, calculate the starting index as one plus the last index in the file
+        if file_exists:
+            existing_df = pd.read_csv(self.csv_file)
+            if not existing_df.empty:
+                # Start from next index after the last one
+                start_index = existing_df.index[-1] + 2
+            else:
+                start_index = 1
+        else:
+            start_index = 1
+
+        # Add an index column starting from the calculated start_index
+        df.index = range(start_index, start_index + len(df))
+
+        # Reset the index to make the index column part of the DataFrame
+        df.reset_index(inplace=True)
+
+        # Write DataFrame to CSV, append if file exists, otherwise write new
+        df.to_csv(self.csv_file, mode='a', index=False, header=not file_exists)
+
+    def remove_by_index(self, index):
+        # Check if the CSV file exists
+        if not os.path.isfile(self.csv_file):
+            print("CSV file does not exist.")
+            return
+
+        # Read the CSV file
+        df = pd.read_csv(self.csv_file)
+
+        # Reset the DataFrame index to align with row numbers starting from 0
+        df.reset_index(drop=True, inplace=True)
+
+        # Adjust the index to match with DataFrame's 0-based indexing
+        adjusted_index = index - 1
+
+        # Check if the adjusted index exists in the DataFrame
+        if adjusted_index not in df.index:
+            print(f"Index {index} not found in the CSV file.")
+            return
+
+        # Remove the row with the specified adjusted index
+        df.drop(adjusted_index, inplace=True)
+
+        # Reset the index to start from 1 after the row is removed
+        df.index = range(1, len(df) + 1)
+
+        # Write the modified DataFrame back to the CSV file
+        df.to_csv(self.csv_file, index=False)
+
+    def modify(self, index, updates):
+        # Check if the CSV file exists
+        if not os.path.isfile(self.csv_file):
+            print("CSV file does not exist.")
+            return
+
+        # Read the CSV file into a DataFrame
+        df = pd.read_csv(self.csv_file)
+
+        # Adjust the index to match with DataFrame's 0-based indexing
+        adjusted_index = index - 1
+
+        # Check if the adjusted index exists in the DataFrame
+        if adjusted_index not in df.index:
+            print(f"Index {index} not found in the CSV file.")
+            return
+
+        # Update the row with the specified adjusted index
+        for key, value in updates.items():
+            if key in df.columns:
+                df.at[adjusted_index, key] = value
+            else:
+                print(f"Column {key} does not exist in the CSV file.")
+
+        # Write the modified DataFrame back to the CSV file
+        df.to_csv(self.csv_file, index=False)
+
+
+# record = Record()
+# record.add([{"date": "2023-11-23", "exercise_name": "bench press",
+#                 "exercise_set": "5", "exercise_rep": "5"}, {"date": "2023-11-23", "exercise_name": "shoulder press",
+#                 "exercise_set": "4", "exercise_rep": "12"}])
+
+# record.remove_by_index(1)
+# record.modify(1, {"exercise_name": "updated exercise", "exercise_set": "6"})
